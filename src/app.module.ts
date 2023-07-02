79491f81d6c5a7563ebe2629a352473a7840ae18
Haesung <koh1260@naver.com>
2023-07-03 05:14:13 +0900

    passport(local, jwt) example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+    })
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
